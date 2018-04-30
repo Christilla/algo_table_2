49d63978ce6e9b0a1edc92aa74f56832166a0a82
Christilla <cpuyjarinet@gmail.com>
2018-04-30 10:13:33 +0200

    affiche la table de 2 en java

---
+++ src/algo_table_2/Algo_table_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algo_table_2;
+import java.util.Scanner;
+/**
+ *
+ * @author Christilla
+ */
+public class Algo_table_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        int value = 2;
+        for(int i = 1; i<=10; i++){
+    System.out.println(i*value);
+    }
+    }
+    
+}
